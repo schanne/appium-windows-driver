2d987882f4ac135b01e3b467022bf9016474d944
Jonathan Lipps <jlipps@gmail.com>
2016-09-21 09:24:08 -0700

    change 'host' to 'address' for better conformance

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const DEFAULT_PORT = 4723;
 
 async function main () {
   let port = yargs.argv.port || DEFAULT_PORT;
-  let host = yargs.argv.host || DEFAULT_HOST;
-  return await startServer(port, host);
+  let address = yargs.argv.address || DEFAULT_HOST;
+  return await startServer(port, address);
 }
 
 if (require.main === module) {
